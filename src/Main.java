06e10f25599803a19694b9b7aa8d5f6118932fe8
ShuEugene <shueugene@hotmail.com>
2022-09-24 10:13:36 +0300

    HW7.1 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+//  Домашние задания к Уроку 7 «Строки»
 public class Main {
     public static void main(String[] args) {
+        hw7_1();
 
+    }
+
+    static void hw7_1() {
+        String firstName = "Ivan", middleName = "Ivanovich", lastName = "Ivanov",
+                fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
     }
 }
